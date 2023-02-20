ac720be6f90fcc4c1d9ff6e1e716e53c2e228db8
Rushik-Ghuntala <rushisoni2003@gmail.com>
2023-02-21 00:18:25 +0530

    add cart page, login page and register page

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
 
 
+//set Templete engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
 //Assets
 app.use(express.static('public'))
 
@@ -13,12 +18,19 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
-//set Templete engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res)=>{
+    res.render('auth/register')
+})
 
 
 app.listen(3300, () => {
     console.log('Listening on Port 3300...')    
-})
+}) 
