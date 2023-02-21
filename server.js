759e942904db114724f9c736fe4c76493b22f980
Rushik-Ghuntala <rushisoni2003@gmail.com>
2023-02-21 13:11:37 +0530

    mongo try

--- server.js
+++ server.js
@@ -3,34 +3,25 @@ const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
-const PORT = process.env.PORT || 3300
+const PORT = process.env.PORT || 3333
 
+const mongoose = require('mongoose')
+
+
+//Database connection
+
+
+
+//Assets
+app.use(express.static('public'))
 
 //set Templete engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-//Assets
-app.use(express.static('public'))
+require('./routes/web')(app)
 
-app.get('/', (req, res) => {
-    res.render('home.ejs')
-})
-
-app.get('/cart', (req, res)=>{
-    res.render('customers/cart')
-})
-
-app.get('/login', (req, res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register', (req, res)=>{
-    res.render('auth/register')
-})
-
-
-app.listen(3300, () => {
-    console.log('Listening on Port 3300...')    
+app.listen(3333, () => {
+    console.log('Listening on Port 3333 ...')    
 }) 
